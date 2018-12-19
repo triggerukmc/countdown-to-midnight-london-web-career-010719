2978688d37546a35fdb2efced8fc07315920d631
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-19 00:17:40 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  first = x.to_i
+  while first > 0
+  puts "#{first} SECOND(S)!"
+  first -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  first = x.to_i
+  while first > 0
+  puts "#{first} SECOND(S)!"
+  first -= 1
+  sleep 1
+end
+return "HAPPY NEW YEAR!"
 end
